1584e909609822e59858a8b51ce5f4d43e4acda1
Alişan <alisankarababa@gmail.com>
2023-11-17 14:24:35 +0300

    add class Employee and tests

---
+++ src/com/workintech/models/Employee.java
@@ -0,0 +1,89 @@
+package com.workintech.models;
+
+import java.util.Arrays;
+import java.util.SimpleTimeZone;
+
+public class Employee {
+
+
+    private int id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String[] healthplans;
+
+    public Employee(int id, String fullName, String password, String email, String[] healthplans) {
+        this.id = id;
+        this.fullName = fullName;
+        this.password = password;
+        this.email = email;
+        this.healthplans = healthplans;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String[] getHealthplans() {
+        return healthplans;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setHealthplans(String[] healthplans) {
+        this.healthplans = healthplans;
+    }
+
+    public void addHealthPlan(int index, String name) {
+
+        if(index > healthplans.length - 1 ) {
+
+            System.out.println("addHealthPlan: index out of bounds");
+            return;
+        }
+
+        if(null != healthplans[index]) {
+            System.out.println("addHealthPlan: index " + index + " is not empty.");
+            return;
+        }
+
+        healthplans[index] = name;
+
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", healthplans=" + Arrays.toString(healthplans) +
+                '}';
+    }
+
+}
--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -1,7 +1,11 @@
 package employeeApp;
 
+import com.workintech.models.Employee;
 import com.workintech.models.Healthplan;
 import com.workintech.models.Plan;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
 
 /**
  * Uygulamanın amacı Java'da nesne tabanlı programlamanın temellerinin oluşturulmasıdır.
@@ -9,7 +13,6 @@ import com.workintech.models.Plan;
  */
 public class Main
 {
-    static Healthplan healthplan = new Healthplan(1, "healthPlan1", Plan.PLAN1);
 
     /**
      * Uygulamadaki tüm işlemler bu metotda yapılmalı
@@ -36,6 +39,48 @@ public class Main
         healthplan.setPlan(Plan.PLAN3);
         System.out.println("healthplan.setPlan(Plan.PLAN3) called");
         System.out.println("healthplan.getPlan(): " + healthplan.getPlan());
+
+        System.out.println("-------------------------------");
+
+        System.out.println("employee-------------------------------");
+        Employee employee = new Employee(11, "employee fullname", "qwerty",
+                "email@email.com", new String[4]);
+
+        System.out.println(employee);
+
+        System.out.println("employee.getId(): " + employee.getId());
+        System.out.println("employee.getFullName(): " + employee.getFullName());
+        System.out.println("employee.getEmail(): " + employee.getEmail());
+        System.out.println("employee.getPassword(): " + employee.getPassword());
+        System.out.println("Arrays.toString(employee.getHealthplans()): " + Arrays.toString(employee.getHealthplans()));
+
+        employee.setId(999);
+        System.out.println("called employee.setId(999)");
+        System.out.println("employee.getId(): " + employee.getId());
+
+        employee.setFullName("full name changed");
+        System.out.println("called employee.setFullName(\"full name changed\")");
+        System.out.println("employee.getFullName(): " + employee.getFullName());
+
+        employee.setEmail("anotheremail@email.com");
+        System.out.println("called employee.setEmail(\"anotheremail@email.com\")");
+        System.out.println("employee.getEmail(): " + employee.getEmail());
+
+        employee.setPassword("newpassword");
+        System.out.println("called employee.setPassword(\"newpassword\")");
+        System.out.println("called employee.setPassword(\"newpassword\")");
+
+        employee.setHealthplans(new String[]{"hp1", "hp2", null, null, "hp5"});
+        System.out.println("called employee.setHealthplans(new String[]{\"hp1\", \"hp2\", null, null, \"hp5\"})");
+        System.out.println("Arrays.toString(employee.getHealthplans()): " + Arrays.toString(employee.getHealthplans()));
+
+        employee.addHealthPlan(3, "hp4");
+        System.out.println("called employee.addHealthPlan(3, \"hp4\")");
+        System.out.println("Arrays.toString(employee.getHealthplans()): " + Arrays.toString(employee.getHealthplans()));
+        System.out.println("calling employee.addHealthPlan(1, \"hp2\")");
+        employee.addHealthPlan(1, "hp2");
+        System.out.println("calling employee.addHealthPlan(10, \"hp11\")");
+        employee.addHealthPlan(10, "hp11");
 
         System.out.println("-------------------------------");
 
