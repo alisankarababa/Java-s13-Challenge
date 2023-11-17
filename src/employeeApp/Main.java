2ff5c9cebc315c385d34ee123fc49141da5a5641
Alişan <alisankarababa@gmail.com>
2023-11-17 13:29:54 +0300

    add tests Healthplan

--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -1,17 +1,43 @@
 package employeeApp;
 
+import com.workintech.models.Healthplan;
+import com.workintech.models.Plan;
+
 /**
  * Uygulamanın amacı Java'da nesne tabanlı programlamanın temellerinin oluşturulmasıdır.
  *
  */
 public class Main
 {
+    static Healthplan healthplan = new Healthplan(1, "healthPlan1", Plan.PLAN1);
 
     /**
      * Uygulamadaki tüm işlemler bu metotda yapılmalı
      */
     private static void workWithData ()
     {
+        Healthplan healthplan = new Healthplan(1, "healthPlan1", Plan.PLAN1);
+        System.out.println("healthplan-------------------------------");
+
+        System.out.println(healthplan);
+
+        System.out.println("healthplan.getId(): " + healthplan.getId());
+        System.out.println("healthplan.getName(): " + healthplan.getName());
+        System.out.println("healthplan.getPlan(): " + healthplan.getPlan());
+
+        healthplan.setId(33);
+        System.out.println("healthplan.setId(33) called");
+        System.out.println("healthplan.getId(): " + healthplan.getId());
+
+        healthplan.setName("changed healthplan name");
+        System.out.println("healthplan.setName(\"changed healthplan name\") called");
+        System.out.println("healthplan.getName(): " + healthplan.getName());
+
+        healthplan.setPlan(Plan.PLAN3);
+        System.out.println("healthplan.setPlan(Plan.PLAN3) called");
+        System.out.println("healthplan.getPlan(): " + healthplan.getPlan());
+
+        System.out.println("-------------------------------");
 
     }
 
